010a61a3cd09ce2eace8f2cfb39a1c71766f91e0
Kaio Caldeira <kaio.caldeira@projetas.com.br>
2018-08-24 16:48:47 -0300

    adiciona método deletar e alterar

--- cadastro-veiculo-ui-js/app/partials/listagem/ListCtrl.js
+++ cadastro-veiculo-ui-js/app/partials/listagem/ListCtrl.js
@@ -2,19 +2,36 @@ app.controller('getcontroller', function($scope, $http, $location) {
     $scope.submit = true;
     $scope.update = false;
     $scope.cancel = false;
-    $scope.userid = true;
+    $scope.userid = false;
 
-	 $scope.getfunction = function(){
+	
 	 	var url = "http://localhost:8080/veiculos";
 		
 	 	$http.get(url).then(function (response) {
-	 		$scope.response = response.data
+             $scope.response = response.data
 	 	}, function error(response) {
 	 		$scope.postResultMessage = "Error with status: " +  response.statusText;
          });
-     }
+     
 
-    
+         $scope.editUser = function(user) {
+            $scope.user = user;
+            $scope.submit = false;
+            $scope.update = true;
+            $scope.cancel = true;
+            $scope.userid = true;
+        
+          };
+        
+        
+          //cancel Uodate
+          $scope.cancelUpdate = function() {
+            $scope.user = null;
+            $scope.submit = true;
+            $scope.update = false;
+            $scope.cancel = false;
+            $scope.userid = false;
+          };
 
 
 
@@ -26,12 +43,15 @@ app.controller('getcontroller', function($scope, $http, $location) {
     
           method: 'DELETE',
           url: 'http://localhost:8080/veiculos/' + user.id
+          
     
         }).then(function successCallback(response) {
     
           alert("User has deleted Successfully");
+          window.location.reload();
           var index = $scope.users.indexOf(user);
           $scope.users.splice(index, 1);
+         
     
         }, function errorCallback(response) {
     
@@ -40,6 +60,32 @@ app.controller('getcontroller', function($scope, $http, $location) {
         });
     };
 
+    $scope.updateUser = function() {
+
+        var url = "http://localhost:8080/veiculos";
+  
+    var config = {
+            headers : {
+                'Accept': 'text/plain'
+            }
+    }
+    var data =  $scope.user;
+      
+   
+    
+    $http.post(url, data, config).then(function (response) {
+        $scope.postResultMessage = response.data;
+        window.location.reload();
+       
+    }, function error(response) {
+        $scope.postResultMessage = "Error with status: " +  response.statusText;
+    });
+    window.location.reload();
+    $scope.id = "";
+    $scope.marca = "";
+}
+     
+    
 
 
 
@@ -52,6 +98,32 @@ app.controller('getcontroller', function($scope, $http, $location) {
             id: $scope.id
         };
  
+         //Set $scope on Edit button click
+ 
+
+  $scope.submitForm = function(){
+    var url = "http://localhost:8080/veiculos";
+  
+    var config = {
+            headers : {
+                'Accept': 'text/plain'
+            }
+    }
+    var data = {
+        id: $scope.id,
+        marca: $scope.marca
+    };
+    
+    $http.post(url, data, config).then(function (response) {
+        $scope.postResultMessage = response.data;
+       
+    }, function error(response) {
+        $scope.postResultMessage = "Error with status: " +  response.statusText;
+    });
+    
+    $scope.id = "";
+    $scope.marca = "";
+}
      
     }
 });
